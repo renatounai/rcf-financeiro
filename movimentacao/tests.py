bdd11c20ea8c4df24591cde2898d61f2aaf4e64e
renato.pereira <Vida2814>
2022-03-31 17:29:24 -0300

    Início dos testes automatizados

--- movimentacao/tests.py
+++ movimentacao/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+import json
 
-# Create your tests here.
+from .endpoints.forma_pagamento_rest import FormaPagamentoIn
+from .models.forma_pagamento import FormaPagamento
+
+
+class FormaPagamentoTest(TestCase):
+
+    def test_should_get_all_formas_de_pagamento(self):
+        FormaPagamento(descricao="Pix").save()
+        FormaPagamento(descricao="Dinheiro").save()
+
+        response = self.client.get("/api/formas_de_pagamento")
+        formas = json.loads(response.content)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(formas), 2)
+
+        self.assertEqual(formas[0]["descricao"], "Pix")
+        self.assertEqual(formas[1]["descricao"], "Dinheiro")
+
+    def test_shoud_get_a_forma_de_pagamento(self):
+        FormaPagamento(descricao="Pix").save()
+        response = self.client.get("/api/formas_de_pagamento/1")
+        forma_de_pagamento = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(forma_de_pagamento["descricao"], "Pix")
+
+    def test_should_create_a_forma_de_pagamento(self):
+        pix = FormaPagamentoIn(descricao="Pix")
+
+        response = self.client.post("/api/formas_de_pagamento", pix.__dict__, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(FormaPagamento.objects.count(), 1)
+
+
