3c4718bb9895c7feec07301ba21153f2ab3a9bb7
renato.pereira <Vida2814>
2022-04-01 09:18:26 -0300

    Conclusão dos testes para a forma_de_pagamento

--- movimentacao/services/forma_pagamento_service.py
+++ movimentacao/services/forma_pagamento_service.py
@@ -5,9 +5,6 @@ from movimentacao.models.forma_pagamento import FormaPagamento
 
 
 def _validate(forma_pagamento: FormaPagamento) -> None:
-    if not forma_pagamento:
-        raise MovimentacaoErro("A forma de pagamento é obrigatória!")
-
     if not forma_pagamento.descricao or not forma_pagamento.descricao.strip():
         raise MovimentacaoErro("A descrição da forma de pagamento é obrigatória!")
 
--- movimentacao/tests.py
+++ movimentacao/tests.py
@@ -1,5 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
-import json
 
 from .endpoints.forma_pagamento_rest import FormaPagamentoIn
 from .models.forma_pagamento import FormaPagamento
@@ -8,11 +8,11 @@ from .models.forma_pagamento import FormaPagamento
 class FormaPagamentoTest(TestCase):
 
     def test_should_get_all_formas_de_pagamento(self):
-        FormaPagamento(descricao="Pix").save()
-        FormaPagamento(descricao="Dinheiro").save()
+        FormaPagamento.objects.create(descricao="Pix")
+        FormaPagamento.objects.create(descricao="Dinheiro")
 
         response = self.client.get("/api/formas_de_pagamento")
-        formas = json.loads(response.content)
+        formas = response.json()
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(formas), 2)
@@ -20,10 +20,16 @@ class FormaPagamentoTest(TestCase):
         self.assertEqual(formas[0]["descricao"], "Pix")
         self.assertEqual(formas[1]["descricao"], "Dinheiro")
 
+    def test_shoud_return_204_if_nothing_found(self):
+        response = self.client.get("/api/formas_de_pagamento")
+        self.assertEqual(response.status_code, 204)
+
     def test_shoud_get_a_forma_de_pagamento(self):
-        FormaPagamento(descricao="Pix").save()
+        FormaPagamento.objects.create(descricao="Pix")
+
         response = self.client.get("/api/formas_de_pagamento/1")
-        forma_de_pagamento = json.loads(response.content)
+
+        forma_de_pagamento = response.json()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(forma_de_pagamento["descricao"], "Pix")
 
@@ -34,4 +40,32 @@ class FormaPagamentoTest(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(FormaPagamento.objects.count(), 1)
 
+    def test_shoud_raise_error_when_missing_description(self):
+        response = self.client.post("/api/formas_de_pagamento", {"descricao": ""}, content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["message"], "A descrição da forma de pagamento é obrigatória!")
 
+    def test_shoud_raise_error_when_description_is_white_space(self):
+        response = self.client.post("/api/formas_de_pagamento", {"descricao": "     "}, content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["message"], "A descrição da forma de pagamento é obrigatória!")
+
+    def test_shoud_raise_error_when_description_is_null(self):
+        response = self.client.post("/api/formas_de_pagamento", {"descricao": None}, content_type="application/json")
+        self.assertEqual(response.status_code, 422)
+
+    def test_shoud_update_a_forma_de_pagamento(self):
+        FormaPagamento.objects.create(descricao="Pix")
+
+        response = self.client.put("/api/formas_de_pagamento/1", {"descricao": "Dinheiro"}, content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        forma_pagamento = FormaPagamento.objects.get(pk=1)
+        self.assertEqual(forma_pagamento.descricao, "Dinheiro")
+
+    def test_shoud_delete_a_forma_de_pagamento(self):
+        FormaPagamento.objects.create(descricao="Pix")
+
+        response = self.client.delete("/api/formas_de_pagamento/1")
+        self.assertEqual(response.status_code, 200)
+        with self.assertRaises(FormaPagamento.DoesNotExist):
+            FormaPagamento.objects.get(pk=1)
