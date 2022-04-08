4ca88bfdfa4f56055d7b9790c923f8f88e0e6281
renato.pereira <Vida2814>
2022-04-08 11:36:04 -0300

    removendo settings desnecessárias

--- financeiro/settings.py
+++ financeiro/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'phonenumber_field',
     'ninja',
     'movimentacao',
@@ -119,10 +118,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
