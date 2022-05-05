ede0856718e9b324d4eb31980cbf8833a66a4811
Renato Custódio Pereira <renatomcn@gmail.com>
2022-05-05 12:26:06 -0300

    testes para a função is_model_empty

--- tests/utils/test_model_utils.py
+++ tests/utils/test_model_utils.py
@@ -8,3 +8,11 @@ class TestModelUtils(SimpleTestCase):
     def test_is_model_not_empty(self):
         model = Pessoa(id=1)
         self.assertFalse(is_model_empty(model))
+
+    def test_is_model_empty_without_id(self):
+        model = Pessoa()
+        self.assertTrue(is_model_empty(model))
+
+    def test_is_model_empty_when_none(self):
+        model = None
+        self.assertTrue(is_model_empty(model))
